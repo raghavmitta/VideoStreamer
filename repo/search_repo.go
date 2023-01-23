c813d5b0e22fac0f088cc502ee875b1f514ac868
Raghav Mittal <raghavmitta@gmail.com>
2023-01-23 12:06:25 +0530

    added repo to search given query in db

---
+++ repo/search_repo.go
@@ -0,0 +1,19 @@
+package repo
+
+import (
+	"VideoStreamer/config"
+	"database/sql"
+	"log"
+)
+
+func SearchData(key string) *sql.Rows {
+	db := config.ConnectDB()
+	key = "%" + key + "%"
+	results, err := db.Query("SELECT * FROM testdb.video where ((title like ?) or (description like ?)) order by published_at desc;", key, key)
+	if err != nil {
+		log.Println(err.Error())
+		return nil
+	}
+	db.Close()
+	return results
+}
